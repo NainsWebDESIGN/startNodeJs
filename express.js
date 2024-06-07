7c79c75e81e4a64bfec81a536dcfd90ea4039825
Nains <jason20226@gmail.com>
2024-06-07 17:29:47 +0800

    2024-06-7 17:29

--- express.js
+++ express.js
@@ -8,6 +8,17 @@ app.get("/", (req, res) => {
 app.get("/mypath", (req, res) => {
   res.send("This is my path");
 });
+app.get("/ttt", (req, res) => {
+  res.send(
+    JSON.stringify([
+      { ttt: "TTT" },
+      { ttt: "EEE" },
+      { ttt: "SSS" },
+      { ttt: "TTT" },
+    ])
+  );
+  // res.send("ttt");
+});
 app.listen(port, () =>
   console.log(`Server running at http://127.0.0.1:${port}/`)
 );
