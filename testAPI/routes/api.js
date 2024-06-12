4cc06e0f54f5effc399c91fa491dd571ced463af
Nains <jason20226@gmail.com>
2024-06-12 14:34:58 +0800

    2024-06-12 14:34

---
+++ testAPI/routes/api.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var router = express.Router();
+
+// 假資料庫
+const data = [
+  {
+    id: 1,
+    title: "產品資訊",
+  },
+];
+
+/* GET home page. */
+router.get("/product", (req, res, next) => {
+  res.send({
+    sussess: true,
+    data,
+  });
+  res.end();
+});
+
+router.post("/product", (req, res) => {
+  const product = req.body;
+  data.push({
+    id: new Date().getTime(),
+    ...product,
+  });
+
+  res.send({
+    success: true,
+    data,
+  });
+
+  res.end();
+});
+
+router.delete("/product/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  //   res.send({
+  //     success: true,
+  //     data,
+  //   });
+
+  res.end();
+});
+
+module.exports = router;
