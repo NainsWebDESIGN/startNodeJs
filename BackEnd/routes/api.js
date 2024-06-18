9d495f691a4d851ab23234801d690a223fbdffb8
Nains <jason20226@gmail.com>
2024-06-18 17:39:05 +0800

    2024-06-18 17:39

--- BackEnd/routes/api.js
+++ BackEnd/routes/api.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 // 假資料庫
-const data = require("../public/json/data.json");
 const apiController = require("../controllers/apiController");
 
 /* GET home page. */
