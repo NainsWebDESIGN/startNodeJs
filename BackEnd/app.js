040e6f64c1ce9d5258578c70b4172d738d43e5d5
Nains <jason20226@gmail.com>
2024-07-05 18:08:32 +0800

    test CORS

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -14,6 +14,14 @@ var usersRouter = require("./routes/users");
 
 var app = express();
 
+app.all("*", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", ["mytoken", "Content-Type"]);
+  next();
+});
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
