148dbc2247a5b705f0cb7ab1b16f3e9e5c7f2ddd
Nains <jason20226@gmail.com>
2024-07-09 14:27:08 +0800

    formate database void

--- BackEnd/testDatabase.js
+++ BackEnd/testDatabase.js
@@ -1,14 +1,19 @@
 require("dotenv").config(); // 載入.env 檔案
 
-const { MYSQL_HOST, MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_DATABASE } =
-  process.env; // 取得環境變數
-const mysql = require("mysql");
-const connection = mysql.createConnection({
-  host: MYSQL_HOST,
-  user: MYSQL_USERNAME,
-  password: MYSQL_PASSWORD,
-  database: MYSQL_DATABASE,
-});
+const {
+    MYSQL_HOST,
+    MYSQL_PORT,
+    MYSQL_USERNAME,
+    MYSQL_PASSWORD,
+    MYSQL_DATABASE,
+  } = process.env, // 取得環境變數
+  mysql = require("mysql"),
+  connection = mysql.createConnection({
+    host: `${MYSQL_HOST}:${MYSQL_PORT}`,
+    user: MYSQL_USERNAME,
+    password: MYSQL_PASSWORD,
+    database: MYSQL_DATABASE,
+  });
 
 connection.connect();
 
