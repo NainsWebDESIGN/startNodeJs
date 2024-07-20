44bcc5d3fa735734097333261dca726febb922fc
NainsWebDESIGN <jason20226@gmail.com>
2024-07-21 00:27:18 +0800

    先隱藏 app.component 的 router

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { Router } from '@angular/Router';
+// import { Router } from '@angular/Router';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +7,12 @@ import { Router } from '@angular/Router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(@Inject(Router) private router: Router) { }
+  constructor(
+    // @Inject(Router) private router: Router
+  ) { }
   ngOnInit() {
-    if (window.location.href.includes('confrim')) {
-      this.router.navigate(['/front/todos']);
-    }
+    // if (window.location.href.includes('confrim')) {
+    //   this.router.navigate(['/front/todos']);
+    // }
   }
 }
