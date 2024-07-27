44f9b45203ab3cece29f91bd66c3667d9f8f5157
NainsWebDESIGN <jason20226@gmail.com>
2024-07-27 20:18:07 +0800

    add images

--- FrontEnd/src/app/three-js/three-js.component.ts
+++ FrontEnd/src/app/three-js/three-js.component.ts
@@ -25,10 +25,15 @@ export class ThreeJsComponent implements OnInit, AfterViewInit {
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     this.renderer = new THREE.WebGLRenderer();
-    this.renderer.setSize(200, 100);
+    this.renderer.setSize(350, 200);
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const geometry = new THREE.BoxGeometry(3, 3, 3);
+    const textureloader = new THREE.TextureLoader();
+    const texture = textureloader.load("./images/bg.png");
+    // 添加顏色
+    // const material = new THREE.MeshBasicMaterial({ color: 0x5C1896 });
+    // 添加材質
+    const material = new THREE.MeshBasicMaterial({ map: texture });
     const cube = new THREE.Mesh(geometry, material);
     this.scene.add(cube);
 
